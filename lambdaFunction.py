1797e100be5bb53013fd115e8d38988817ba1f07
Alan Zambrano <azambrano@ilsp.com.mx>
2021-12-08 11:02:26 -0600

    fix: delete __init__.py

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -13,31 +13,36 @@ MainClass = Main()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+
 def splitPhoneNumber(phoneNumber):
     response = {}
     if phoneNumber[0] == '+':
         phoneNumber = phoneNumber[3:]
     aux = ""
     for index, number in enumerate(phoneNumber):
-    	if index%2 == 0:
-    		aux += str(number)
-    	else:
-    		aux += str(number) + " "
+        if index % 2 == 0:
+            aux += str(number)
+        else:
+            aux += str(number) + " "
     response["phoneNumber"] = aux
     return response
 
+
 def sendEmail(parameters):
     response = {}
-    sent = MainClass.sendEmail(parameters["emailTo"],parameters["message"],parameters["subject"])
+    sent = MainClass.sendEmail(
+        parameters["emailTo"], parameters["message"], parameters["subject"])
     if sent:
         response["result"] = True
         return response
     else:
         raise Exception('Action not supported')
 
+
 def saveReport(details):
     response = {}
-    conn = pymssql.connect(os.environ['server'], os.environ['user'], os.environ['password'], os.environ['db'])
+    conn = pymssql.connect(
+        os.environ['server'], os.environ['user'], os.environ['password'], os.environ['db'])
     cursor = conn.cursor(as_dict=True)
     cursor.execute("""INSERT INTO calls ([numberPhone],[startCall],[endCall],[offset],[id_poc],[create_date])
     VALUES (%s, %s, null, null, 1, %s)""", (details["ContactData"]["CustomerEndpoint"]["Address"], str(datetime.datetime.now())[:-7], str(datetime.datetime.now())[:-7]))
@@ -45,18 +50,22 @@ def saveReport(details):
     conn.commit()
     return response
 
+
 def updateReport(parameters):
     response = {}
-    conn = pymssql.connect(os.environ['server'], os.environ['user'], os.environ['password'], os.environ['db'])
+    conn = pymssql.connect(
+        os.environ['server'], os.environ['user'], os.environ['password'], os.environ['db'])
     cursor = conn.cursor(as_dict=True)
     cursor.execute("SELECT * FROM calls WHERE id = %s", parameters["id"])
     call = cursor.fetchone()
-    seconds = int((datetime.datetime.now() - call["startCall"]).total_seconds())
-    cursor.execute("UPDATE calls set [endCall]=%s, [offset]=%s WHERE [id] =%s",(str(datetime.datetime.now())[:-7]), seconds)
+    seconds = int((datetime.datetime.now() -
+                  call["startCall"]).total_seconds())
+    cursor.execute("UPDATE calls set [endCall]=%s, [offset]=%s WHERE [id] =%s", (str(
+        datetime.datetime.now())[:-7]), seconds)
     conn.commit()
     response["result"] = True
     return response
-    
+
 
 def dispatch(event):
     if 'Details' in event:
@@ -75,6 +84,7 @@ def dispatch(event):
                     return updateReport(event["Details"]["Parameters"])
 
     raise Exception('Action not supported')
+
 
 def lambdaHandler(event, context):
     logger.debug('event={}'.format(json.dumps(event)))
